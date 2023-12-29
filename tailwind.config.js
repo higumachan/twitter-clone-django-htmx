12ef86e934d3b2a66bc67fc6a4016f5d1122ded8
Yuta Hinokuma <higumachan@users.noreply.github.com>
2023-12-29 17:46:07 +0900

    ツイート時にアニメーションを入れる

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,32 @@ module.exports = {
     content: ['./**/templates/*.html', './**/templates/**/*.html'],
   },
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "slide-bottom": "key-slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)    both",
+        "scale-up-ver-top": "scale-up-ver-top 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both"
+      },
+      keyframes: {
+        "key-slide-bottom": {
+          "0%": {
+            transform: "translateY(-100%)"
+          },
+          to: {
+            transform: "translateY(0)"
+          }
+        },
+       "scale-up-ver-top": {
+         "0%": {
+           transform: "scaleY(0.0)",
+           "transform-origin": "100% 0%"
+         },
+         to: {
+           transform: "scaleY(1)",
+           "transform-origin": "100% 0%"
+         }
+       }
+      }
+    },
   },
   variants: {},
   plugins: [],
